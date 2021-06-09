73735f3d734a8e42c3103091196de066e07477fc
Ben <mellonbenjamin@gmail.com>
2021-06-09 18:11:22 +0200

    update fonctionnel par Dark le bo.

--- bot.js
+++ bot.js
@@ -3,55 +3,59 @@ const bot = new Discord.Client();
 const config = require("./cfg.json");
 const fs = require('fs');
 
-// const channel = bot.channels.cache.find(channel => channel.name === "logs");
-
 function timeNow()
 {
-	var d = new Date();
-	i = d.toLocaleTimeString();
-	return i;
+    const d = new Date();
+    function pad(s) { return (s < 10) ? '0' + s : s; }
+    let i = [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/')
+    i += " à " + d.toLocaleTimeString('fr', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZone: 'Europe/Paris'
+    });
+    return i;
 }
 
-var time;
-var tmp;
-var logs;
-
-
+let time;
+let tmp;
+let logs;
 
 bot.on('ready', () => {
-	console.log('Connected and ready.');
+    console.log('Connected and ready.');
 });
 
 bot.on('message', (msg) => {
 
-	if (msg.author.bot) return;
+    if (msg.author.bot) return;
 
-	if (msg.mentions.has(bot.user) && !msg.content.includes("[anonyme]"))
-		msg.channel.send("usage : [anonyme] *votre message*");
+    if (msg.mentions.has(bot.user) && !msg.content.includes("[anonyme]"))
+        msg.channel.send("usage : [anonyme] *votre message*");
 
-	if (msg.content.includes("[anonyme]"))
-	{
-		time = timeNow();
-		logs = tmp = msg.content;
-		tmp = tmp.replace('[anonyme]', '');
-		if (msg.attachments.size) {
-			for (const attachment of msg.attachments.values()) {
-				msg.channel.send(tmp, attachment.filesize > 8000000 ? attachment.url : { files: [attachment.url] });
-			}
-		}
-		else
-		{
-			try {
-				msg.channel.send(tmp);
-			} catch (err) {
-				console.warn('Erreur : remplacement impossible.');
-				console.warn(err);
-			}
-		}
-		msg.delete({ timeout: 10 })
-		.then(message => bot.channels.cache.get(config.channelid).send('[' + time + ']' + ' ' + msg.author.username + ' : ' + logs))
-		.catch(console.error);
-	}
+    if (msg.content.includes("[anonyme]"))
+    {
+        time = timeNow();
+        tmp = msg.content;
+        logs = tmp = tmp.replace('[anonyme]', '').trim();
+        tmp = "**Message posté anonymement**\n" + tmp
+        if (msg.attachments.size) {
+            for (const attachment of msg.attachments.values()) {
+                msg.channel.send(tmp, attachment.filesize > 8000000 ? attachment.url : { files: [attachment.url] });
+            }
+        }
+        else
+        {
+            try {
+                msg.channel.send(tmp);
+            } catch (err) {
+                console.warn('Erreur : remplacement impossible.');
+                console.warn(err);
+            }
+        }
+        msg.delete({ timeout: 10 })
+        .then(message => bot.channels.cache.get(config.channelid).send(`*Posté par ${msg.author}, le ${time}, dans le canal ${msg.channel}*\n\`${logs}\``))
+        .catch(console.error);
+    }
 });
 
 bot.on('error', err => {
